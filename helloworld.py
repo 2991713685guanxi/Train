0fec184ad7836424e61725f18da4d9cbe0d36999
2991713685guanxi <2991713685guanxi@gmail.com>
2026-09-24 16:31:10 +0800

    13

---
+++ helloworld.py
@@ -0,0 +1,21 @@
+"""
+a= 3.14
+b = []
+print(a)
+
+print(f"我今年{a}hhhe")
+c = int(a)
+print(type(c))
+age = 18
+bbb = "很牛" if age >= 18 else "很拉"
+print(bbb)
+"""
+b = []
+while True:
+    a = [0]
+    for i in range(1,10):
+        a.append(i)
+    b.append(a)
+    print(b)
+    if len(b) == 100:
+        break
